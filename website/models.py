32af4603aafc5882de311717704438fc974b6fe3
TrentSugars <n11274328@qut.edu.au>
2024-10-29 14:16:42 +1000

    Updated class User and Comment for db

--- website/models.py
+++ website/models.py
@@ -4,8 +4,35 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
 
-class Event(db.Model):
+    __tablename__ = 'users' # good practice to specify table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+	#password is never stored in the DB, an encrypted password is stored
+	# the storage should be at least 255 chars long
+    password_hash = db.Column(db.String(255), nullable=False)
+    # relation to call user.comments and comment.name
+    comments = db.relationship('Comment', backref='user')
+
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Comment(db.Model):
 
-class Order(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    # add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
+
+    # string print method
+    def __repr__(self):
+        return f"Comment: {self.text}"
+
+
+#Need to add code for these
+#class Event(db.Model):
+#class Order(db.Model):
